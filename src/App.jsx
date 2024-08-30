be6faf97a7323d4563d17a15028fb0fa440151f8
Abhiram Vakil <abhiramvakil@gmail.com>
2024-08-30 11:40:54 +0530

    events page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import './App.css';
+import Events from './pages/Events/Events';
 
 function App() {
 
@@ -10,9 +11,9 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home/>} />
-          <Route path='/events' element={<h1>Events</h1>} />
           <Route path='/about' element={<h1>About</h1>} />
           <Route path='/contact' element={<h1>Contact</h1>} />
+          <Route path='/events' element={<Events/>} />
           <Route path='sponsor' element={<h1>Sponsor</h1>} />
         </Routes>
       </Router>
